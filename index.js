2e1a456fa9957c316dd96f6cc185cbe32293a3e7
vsmith95 <vaughnsmith95@gmail.com>
2022-05-23 23:08:29 -0400

    index.js should be done, testing the app shows that the program runs in console, prompts the user with questions and then logs said questions and creates README, does not currently populate the readme

--- index.js
+++ index.js
@@ -1,13 +1,168 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project Title?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license are you using?',
+        choices: ['none', 'MIT', 'Creative Commons Attribution 4.0', 'Do What The F*ck You Want To Public License', 'Apache license 2.0'],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log("Please enter one of the listed options.")
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a description of your project.',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a description.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'instalation',
+        message: 'Enter any instalation instructions required for your project.',
+        validate: instalationInput => {
+            if (instalationInput) {
+                return true;
+            } else {
+                console.log('Please enter instalation instructions.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please tell how your project is used.',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please provide how your project is used.');
+                return false;
+            }
+        }
+
+    },
+
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide any contribution guidelines for your project.',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please provide contribution guidelines.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'Please enter test instructions for your project.',
+        validate: instructionsInput => {
+            if (instructionsInput) {
+                return true;
+            } else {
+                console.log('Please provide the test instructions for your project.');
+                return false;
+            }
+        }
+
+    },
+    
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please enter your GitHub username.',
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+                console.log('Please provide your GitHub username.');
+                return false;
+            }
+        }
+
+    },
+    
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address.',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please provide your email.');
+                return false;
+            }
+        }
+
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./output/generatedREADME.md', fileName, data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            
+            resolve({
+                ok: true
+            });
+        })
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(answers) {
+        console.log(answers);
+        var data = generateMarkdown(answers);
+        writeToFile(data)
+    });
+}
 
 // Function call to initialize app
 init();
